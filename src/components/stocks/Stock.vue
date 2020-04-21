06d77d13740412f34865ccab0e181df0addca5c6
LaupWing <loc_strict@hotmail.com>
2020-04-21 12:09:33 +0200

    289

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,30 @@
 <template>
-    <h1>Stock</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-succes">
+            <div class="panel-header">
+                <h3 class="panel-title">
+                    NAME
+                    <small>(Price: PRICE)</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input 
+                        type="number"
+                        class="form-control"
+                        placeholder="Quantity"
+                    >
+                </div>
+                <div class="pull-right">
+                    <button 
+                        class="btn btn-succes"
+                    >
+                        Buy
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
-
+        <app-stock v-for='stock in stocks' :key='stock.id'/>
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+    import Stock from './Stock';
+    export default {
+        components:{
+            appStock: Stock
+        },
+        data(){
+            return{
+                stocks:[
+                    {
+                        id: 1,
+                        name: 'BMW',
+                        PRICE: 110
+                    },
+                    {
+                        id: 2,
+                        name: 'Apple',
+                        PRICE: 200
+                    },
+                    {
+                        id: 3,
+                        name: 'Google',
+                        PRICE: 250
+                    },
+                    {
+                        id: 4,
+                        name: 'Twitter',
+                        PRICE: 50
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <style>
